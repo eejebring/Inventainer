b7d3c47796757dec1fb0b66a40ef7100729523bd
eejebring <dev@ejebring.com>
2023-11-20 19:59:20 +0100

    init snapshot

---
+++ main.lua
@@ -0,0 +1,57 @@
+local folder = "inventainer/inv/"
+
+function snapshot(peripheralName, supplierName)
+
+    local newPeripheral = peripheral.wrap(peripheralName)
+    if (newPeripheral == nil) then
+        print("invalid peripheral")
+        return
+    end
+
+    local supplier = peripheral.wrap(supplierName)
+    if supplier == nil then
+        print("invalid supplier")
+        return
+    end
+
+    local snapshot = newPeripheral.list()
+
+    table.insert(snapshot, supplierName)
+    io.output(folder .. peripheralName)
+    
+    for value in snapshot do
+        io.write(value.name .. "\n")
+    end
+
+    io.close()
+end
+
+function maintain()
+    print(fs.list(folder))
+end
+
+function main()
+    print("select an option:\n 1) new snapshot\n 2) maintain inventories\n")
+
+    local running = true
+    while running do
+        running = false
+        local action = read()
+
+        if action == "1" then
+            print("Enter peripheral name")
+            local peripheralName = read()
+            print("Enter supplier name")
+            local supplierName = read()
+            snapshot(peripheralName, supplierName)
+
+        elseif action == "2" then
+            maintain()
+
+        else
+            print("invalid option")
+            running = true
+        end
+    end
+end
+main()
