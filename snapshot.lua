7192957c4d4759b01eaa913866b09ad740bb1f41
eejebring <dev@ejebring.com>
2023-11-23 22:22:29 +0100

    snapshot transfer parameter

--- snapshot.lua
+++ snapshot.lua
@@ -2,10 +2,10 @@ local args = {...}
 require("api")
 
 local function main()
-    if #args == 2 then
-        Snapshot(args[1], args[2])
+    if (#args == 2) or (#args == 3) then
+        Snapshot(args[1], args[2], args[3])
     else
-        print("Two arguments required: the inventory, and the supplier name")
+        print("Two or three arguments required: the inventory, and the supplier name, [optional] transfer name")
     end
 end
 main()
